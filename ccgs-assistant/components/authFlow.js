5918dbfa6f94a98dc44fbf5ced1d477898d409a0
Yavonix <37795234+Namor888@users.noreply.github.com>
2020-08-08 23:38:54 +0800

    Updated navbar
    + Scaling issue resolved with flexbox.
    (LOL)
    
    Co-Authored-By: kenneth4562 <39127066+kenneth4562@users.noreply.github.com>

--- ccgs-assistant/components/authFlow.js
+++ ccgs-assistant/components/authFlow.js
@@ -1,11 +1,10 @@
 import { useRouter } from 'next/router'
 import URLSafeBase64 from 'urlsafe-base64'
-import styles from "../styles/nav.module.css"
 
 function Login({ children }) {
     const router = useRouter()
     return (
-        <a className={styles.sioButton} href={"/api/login?redirect=" + URLSafeBase64.encode(Buffer.from(router.pathname))}>
+        <a href={"/api/login?redirect=" + URLSafeBase64.encode(Buffer.from(router.pathname))}>
         {children}
         </a>
     )
@@ -13,7 +12,7 @@ function Login({ children }) {
 
 function Logout({ children }) {
     return (
-        <a className={styles.sioButton} href={"/api/logout"}>
+        <a href={"/api/logout"}>
         {children}
         </a>
     )
--- ccgs-assistant/components/layout.js
+++ ccgs-assistant/components/layout.js
@@ -24,58 +24,57 @@ function Layout({ children }) {
          }
          <div className={styles.all}>
           <div className={styles.sideNav}>
-            <div className={styles.logo}>
-              <div className={styles.imageContainer}>
-                <img src={logo} className={styles.image}/>
-              </div>
-            </div>
-            <div className={styles.links}>
-              <Link href="/">
-                <a>Dashboard</a>
-              </Link>
-              <Link href="/diary">
-                <a>Diary</a>
-              </Link>
-              <Link href="/timetable">
-                <a>Timetable</a>
-              </Link>
-              <Link href="/map">
-                <a>Campus map</a>
-              </Link>
-              <Link href="/info">
-                <a>School information</a>
-              </Link>
-              <Link href="/vote">
-                <a>Voting</a>
-              </Link>
-              <Link href="/settings">
-                <a>Settings</a>
-              </Link>
-            </div>
+
+            <img src={logo} className={styles.image}/>
+
+            <Link href="/">
+              <a>Dashboard</a>
+            </Link>
+            <Link href="/diary">
+              <a>Diary</a>
+            </Link>
+            <Link href="/timetable">
+              <a>Timetable</a>
+            </Link>
+            <Link href="/map">
+              <a>Campus map</a>
+            </Link>
+            <Link href="/info">
+              <a>School information</a>
+            </Link>
+            <Link href="/vote">
+              <a>Voting</a>
+            </Link>
+            <Link href="/settings">
+              <a>Settings</a>
+            </Link>
+
             <div className={styles.profileArea}>
-              <div className={styles.profileImgArea}>
-                <img src={user ? user.picture : na} className={styles.profileImage}/>
-              </div>
+
+              <img src={user ? user.picture : na} className={styles.profileImage}/>
+
               <div className={styles.profileTextArea}>
-              <p className={styles.profileText}>{user ? user["https://aad.com/DisplayName"] : <>User currently not signed in</>}</p>
+              <p className={styles.profileText}>{user ? user["https://aad.com/DisplayName"] : <>User </>}</p>
                 {loading ? <>LOADING</> :
                 <>
                 {user ? <>
                   <Logout>
-                    Logout
+                    <button className={styles.sioButton}>Logout</button>
                   </Logout>
                 </>:<>
                   <Login>
-                    Login
+                    <button>Login</button>
                   </Login>
                 </>}
                 </>
                 }
               </div>
             </div>
+
+
           </div>
               
-          <main><div>{children}</div></main>
+          <main className={styles.mainC}><div>{children}</div></main>
           </div>
         </>
     )
