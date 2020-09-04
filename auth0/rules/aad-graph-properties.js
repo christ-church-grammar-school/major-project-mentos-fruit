4f3fa41ccd4f53315f3d7ec5ad59352f8c279ec1
Yavonix <37795234+Namor888@users.noreply.github.com>
2020-09-04 18:47:42 +0800

    Auth0 Cleanup
    Added boarding check and moved hard coded values to environment variables

--- auth0/rules/aad-graph-properties.js
+++ auth0/rules/aad-graph-properties.js
@@ -2,10 +2,39 @@ function addAttributes(user, context, callback) {
     const request = require('request');
     var identityLength = user.identities.length;
     const namespace = 'https://aad.com/';
+    var completion = 0;
+
+    function requestCallback() {
+        if(completion === 2) {
+            callback(null, user, context);
+        }
+    }
+
     for (var i = 0; i < identityLength; i++) {
         if (user.identities[i].provider === 'waad') {
             //context.idToken[namespace+'access_token'] = user.identities[i].access_token;
             //context.idToken[namespace+'id'] = user.identities[i].user_id;
+            request({
+                url: 'https://graph.windows.net/me/memberOf?api-version=1.6',
+                headers: {
+                    'Authorization': 'Bearer ' + user.identities[i].access_token
+                }
+            }, (error, response, body) => {
+                var metadata = JSON.parse(response.body);
+                var boarding = "false";
+                for (var i = 0; i < metadata.value.length; i++) {
+                    if (metadata.value[i].displayName === 'BOARDING') {
+                        boarding = "true";
+                    }
+                }
+                context.idToken[namespace + 'Boarding'] = boarding;
+                if (error) {
+                    console.error(`MemberOfError: ${error}`);
+                }
+                completion ++;
+                requestCallback();
+            });
+
             request({
                 url: 'https://graph.windows.net/me?api-version=1.6',
                 headers: {
@@ -18,9 +47,11 @@ function addAttributes(user, context, callback) {
               	context.idToken[namespace + 'YearLevel'] = metadata.jobTitle;
               	context.idToken[namespace + 'EmployeeID'] = metadata.employeeId;
                 context.idToken[namespace + 'DisplayName'] = metadata.displayName;
-                console.error('error:', error); // Print the error if one occurred
-                //console.log(JSON.parse(response.body));
-                callback(null, user, context);
+                if (error) {
+                    console.error(`Me: ${error}`);
+                }
+                completion ++;
+                requestCallback();
             });
         }
     }
--- ccgs-assistant/pages/api/login.js
+++ ccgs-assistant/pages/api/login.js
@@ -3,7 +3,7 @@ import URLSafeBase64 from 'urlsafe-base64'
 
 export default async function login(req, res) {
    try {
-     await auth0.handleLogin(req, res, { redirectTo: URLSafeBase64.decode(req.query.redirect).toString('utf8'), authParams: {connection : "rgtesting123"} })
+     await auth0.handleLogin(req, res, { redirectTo: URLSafeBase64.decode(req.query.redirect).toString('utf8'), authParams: {connection : process.env.NEXT_PUBLIC_DEFAULT_CONNECTION} })
    } catch (error) {
      console.error(error)
      res.status(error.status || 500).end(error.message)
