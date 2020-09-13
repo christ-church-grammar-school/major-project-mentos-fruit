1242f09f503db83b5e578afe7dc7399b4f903a2b
VestingHarp <39127066+VestingHarp@users.noreply.github.com>
2020-09-13 21:39:45 +0800

    Voting Homepage Added
    + Voting subheadings
    - boxes
    + Styled buttons
    + Made navbar unhighlightable

--- ccgs-assistant/pages/diary.js
+++ ccgs-assistant/pages/diary.js
@@ -1,10 +1,17 @@
 import { getLayout } from '../components/layout'
+import { useFetchUser } from '../lib/user'
 
 function Diary() {
+    const { user, loading } = useFetchUser()
     return (
-        <div>
-            <img src="PAGES/Diary.png" className="placeholder"/>
-        </div>
+        <>
+        {loading ? <>LOADING</> : <>
+          {!user ? <p>User Not Signed In. Please sign in at the bottom right.</p> : <>
+            <div>
+                <img src="PAGES/Diary.png" className="placeholder"/>
+            </div>
+        </>}</>}
+        </>
     )
 }
 
--- ccgs-assistant/pages/index.js
+++ ccgs-assistant/pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import Link from 'next/link'
-//import { useFetchUser } from '../lib/user'
+import { useFetchUser } from '../lib/user'
 import { getLayout } from '../components/layout'
 
 function Home() {
-  //const { user, loading } = useFetchUser()
+  const { user, loading } = useFetchUser()
 
   return (
-      <div>
-        <img src="PAGES/Dashboard.png" className="placeholder"/>
-      </div>
+      <>
+      {loading ? <>LOADING</> : <>
+        {!user ? <p>User Not Signed In. Please sign in at the bottom right.</p> : <>
+          <div>
+            <img src="PAGES/Dashboard.png" className="placeholder"/>
+          </div>
+      </>}</>}
+      </>
   )
 }
 
