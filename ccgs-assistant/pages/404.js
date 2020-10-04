7a0288252f47e5aeb4b163ad6df99d83a7aed9e9
VestingHarp <39127066+VestingHarp@users.noreply.github.com>
2020-10-04 21:42:05 +0800

    Created ionic app

--- ccgs-assistant/pages/404.js
+++
@@ -1,9 +0,0 @@
-import Error from 'next/error'
-
-function LostPage({ statusCode }) {
-    return <Error statusCode={404}/>
-}
-
-LostPage.getLayout = page => {return page}
-
-export default LostPage
