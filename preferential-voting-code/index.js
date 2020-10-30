2d1585c12b5d120886bfdb41112f94531250f1f3
1015634 <62336282+1015634@users.noreply.github.com>
2020-10-30 23:02:22 +0800

    Add GroupCandidate table
    Also filter candidates for preferential voting algorithm

--- preferential-voting-code/index.js
+++ preferential-voting-code/index.js
@@ -20,14 +20,14 @@ const adapter = new FileSync('votes.json')
 const db = low(adapter)
 
 function initdb() {
-	db.defaults({ candidate: [], vote: []})
+	db.defaults({ candidate: [], groupcandidate: [], vote: []})
 	.write()
 }
 
 function addCandidate(candid, sname, syear, sbio) {
+	// Add information about candidate
 	if (db.get('candidate').find({ studentid: candid}).value() == undefined) {
 		// Create new user
-		console.log('test1')
 		db.get('candidate')
 		.push({ studentid: candid, name: sname, year: syear, bio: sbio })
 		.write()
@@ -40,6 +40,15 @@ function addCandidate(candid, sname, syear, sbio) {
 	}
 }
 
+function addCandGroup(candidateid, grouptype) {
+	// Add candidate to a group
+	if (db.get('groupcandidate').find({ studentid: candidateid, group: grouptype }).value() == undefined) {
+		// (Candidateid, grouptype) does not exist in table
+		db.get('groupcandidate')
+		.push( { studentid: candidateid, group: grouptype } )
+		.write()
+	}
+}
 function addVote(id, grouptype, candidateid, preference) {
 	// Add vote
 	if (db.get('vote').find({ voterid: id, group:grouptype, cand:candidateid }).value() != undefined) {
@@ -49,11 +58,35 @@ function addVote(id, grouptype, candidateid, preference) {
 	// Add vote
 	db.get('vote')
 	.push({ voterid: id, group:grouptype, cand:candidateid, pref:preference }) 
-	.write()
+	.write() 
+}
+
+function calcResults(curyear, grouptype) {
+	// Perform preferential voting algorithm
+
+	// allcandidates contains all candidates in correct year
+	allcandidates = db.get('candidate')
+	.filter({ year: curyear })
+	.map('studentid')
+	.value()
+	candidates = []; // Candidates for this particular group
+	for (var i=0; i<allcandidates.length; i++) {
+		if (db.get('groupcandidate').find({ studentid: allcandidates[i], group: grouptype }).value()) {
+			candidates.push(allcandidates[i]) // Add candidate
+		}
+	}
+	console.log(candidates)
 }
 
 initdb()
 
 // test database
 addCandidate('test@student.ccgs.wa.edu.au', 'Fname Lname', 2020, 'I am dumb')
+addCandidate('test2@student.ccgs.wa.edu.au', 'Fname2 Lname2', 2020, 'I am dumber')
+addCandidate('test3@student.ccgs.wa.edu.au', 'Fname3 Lname3', 2019, 'I am dumbest')
+addCandGroup('test@student.ccgs.wa.edu.au', 'Wolsey')
+addCandGroup('test@student.ccgs.wa.edu.au', 'School')
+addCandGroup('test2@student.ccgs.wa.edu.au', 'Romsey')
+addCandGroup('test3@student.ccgs.wa.edu.au', 'Wolsey')
 addVote('1015634@student.ccgs.wa.edu.au', 'Wolsey', 'test@student.ccgs.wa.edu.au', 1);
+calcResults(2020, 'Wolsey')
